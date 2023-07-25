5d7a545db881f307fb0096d9acf3a177a2b46f8e
Iryna Stetsiura <st.linden.11@gmail.com>
2023-07-25 15:34:20 +0200

    hw15,16(2,3)

--- js/15(ap,promise)/script.js
+++ js/15(ap,promise)/script.js
@@ -1,23 +1,35 @@
 {
-    
-}//fetch basic
+    function confirmPromise(text) {
+        function executor(resolve, reject) {
+            const result = confirm(text);
+            if (result) {
+                resolve();
+            } else {
+                reject();
+            }
+        }
+        return new Promise(executor);
+    }
 
+    confirmPromise('Проміси це складно?').then(
+        () => console.log('не так вже й складно'),
+        () => console.log('respect за посидючість і уважність')
+    );
 
+}//Promisify: confirm
 
-{}//fetch improved
-
-
-
-{}//race
-
-
-
-{}//Promisify: confirm
-
-
-
-{}//Promisify: prompt
-
-
-
-{}//Promisify: LoginForm
+{ 
+    function promptPromise(text){
+        function executor(resolve, reject) {
+            const name = prompt(text);
+            if (name) {
+                resolve(name);
+            } else {
+                reject();
+            }
+        }
+        return new Promise(executor);
+   }
+   promptPromise("Як тебе звуть?").then(name => console.log(`Тебе звуть ${name}`),
+                                          () => console.log('Ну навіщо морозитися, нормально ж спілкувалися'))
+}//Promisify: prompt
